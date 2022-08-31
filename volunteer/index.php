732d6d2252bb9c959ad6c6ecedfe460e9e17b7ac
Jonas Anker Rasmussen <jonasanker@gmail.com>
2022-08-31 10:40:57 +0200

    Sync files per td

--- volunteer/index.php
+++ volunteer/index.php
@@ -25,23 +25,24 @@
 			
 			<h1>What is Café Analog?</h1>
 			<p>Analog is ITU’s student-run, non-profit café, serving coffee and tea to more than 500 students a day. We’re comprised of about 100 students of all majors, nationalities and ages, working together to make Analog the best, most “hygge”, place on ITU.</p>
-			<p>Analog is an organization with a flat structure. We elect a new board, called Analogen, every semester, but we are at no different level than any other volunteer -- we all started as baristas just like you. </p>
+			<p>Analog is an organization with a flat structure. We elect a new board, called Analogen, every semester, but we are at no different level than any other volunteer - we all started as baristas just like you. </p>
 			<h1>What we expect from you as a volunteer</h1>
-			<p>Keeping a café open from 7:45-18 (Friday's to 15) (we close at 16 this semester due to corona) every week day is hard work, and so, volunteering in Analog requires trust. We need reliable, enthusiastic people to join our organization. We expect you to take initiative, and to consult your fellow volunteers about your ideas. Anyone in Analog can improve on what we do, but being about 100 people in an organization means that we need to improve upon ourselves collaboratively.</p>
+			<p>Keeping a café open from 7:45-16 (Friday's to 14) every week day is hard work, and so, volunteering in Analog requires trust. We need reliable, enthusiastic people to join our organization. We expect you to take initiative, and to consult your fellow volunteers about your ideas. Anyone in Analog can improve on what we do, but being about 100 people in an organization means that we need to improve upon ourselves collaboratively.</p>
 			<p>We have various roles that need filling, but most of our volunteers are baristas, i.e. they take shifts and make coffee. Other volunteers plan our shifts, manage our storage, plan our monthly hangouts, audit our financial accounts and so on. Everyone has to go through barista training though, but as you become more involved in Analog, you can move onto other responsibilities. As a barista, you are expected to take <b>one shift of ~two hours every week</b>. The other roles have varying degrees of expected work load. </p>
 			<h1>What’s in it for you?</h1>
-			<p>Volunteering in Analog is hard work, but it also comes with some great benefits. First of all, you join the Analog family, which means a chance to meet the greatest people at ITU across majors and classes. We get together once a month to play games, hang out, share tips and tricks and talk about Analog business.</p>
+			<p>Volunteering in Analog is hard work, but it also comes with some great benefits. First of all, you join the Analog family, which means a chance to meet the greatest people at ITU across majors and classes. We get together several times per semester to hang out and get to know each other better. For example we've played pentanque at BouleBar, painted mugs at Creative Space and played mini golf at Camping Bar. We also host events in the café for all of ITU.</p>
 			<p>Additionally, all volunteers can enjoy free black coffee and tea, as well as espresso drinks at a heavily reduced price.</p>
-			<p>In Analog we use Facebook to communicate, and so we expect you to check and actively engage with the Facebook group regularly. </p>
+			<p>In Analog we use Facebook to communicate, and so we expect you to check and actively engage with the Facebook group regularly. We are however in the fall semester 2022 planning on switching from Facebook to another platform. If accepted you will of course be notified hereof. </p>
 			<h1>How to apply</h1>
 			<p>We’re excited that you have gotten this far, and are still interested in joining our family. If you do however have any unanswered questions, you are welcome to get in touch with Analog’s board at <b><em>analogen[at]cafeanalog[dot]dk</em></b>.</p>
           <p>By applying, you are also telling us that you’ve <u>read and understood</u>, what being an Analog member requires.</p>
           	<p>To apply, complete the following steps: 
             <ol style="font-family: Georgia, Serif; font-size: 120%; line-height:140% ">  
             <li>Fill out the form below.</li>
-          	<li>Make a short video of yourself telling us a little bit about yourself, and why you want to volunteer in Analog.</li>
+          	<li>Make a short video of yourself telling us a little bit about yourself. We want to get to know you - so focus on that, as well as why you want to volunteer in Analog.</li>
             <li>Upload the video with your application below.</li>
           	</ol>
+          	<p>The deadline for application in Fall 2022 is: September 11th at midnight (23.59)
           	<h1>Your application video</h1>
           	<p>To get to know you better, we would like to hear why you think you would fit right in to the Analog crew. Maybe you have made a funny and interesting project that you would like to tell us about? Maybe you have a suggestion for how to make Analog better, or maybe you have a good joke, that you would like to tell us. 
               <br/>This is a great opportunity to show us your personality and give us a taste of who you are in a short and consise format.
